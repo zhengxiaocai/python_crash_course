7e53a844bdcc8ab4d0a3ae042dc9fcf0c5007f48
zhengxiaocai <zhengyuan_tester@163.com>
2020-05-08 17:46:57 +0800

    split()

--- py10/py1006_FileNotFoundError.py
+++ py10/py1006_FileNotFoundError.py
@@ -5,4 +5,14 @@ if __name__ == '__main__':
         with open(filename, 'r') as f:
             f.read()
     except FileNotFoundError as e:
+        print('File Not Found.', e)
+
+    alice = 'programming.txt'
+    try:
+        with open(alice, 'r') as f:
+            contents = f.read()
+    except FileNotFoundError as e:
         print('File Not Found.')
+    else:
+        contents = contents.split()
+        print(f'Len is {len(contents)}')
